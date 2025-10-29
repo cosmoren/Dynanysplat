bd21e9fd3cdc1ff3ca83b68d12d86121dacf2366
Yuan Ren <cosmo.ren@gmail.com>
2025-10-29 14:02:13 +0000

    Add an additional loss term for penalizing similarity between dynamic and static rendering, regularized dynamic rendering regions but fail in guiding it to focus the dynamic part

--- src/loss/loss_mse.py
+++ src/loss/loss_mse.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from jaxtyping import Float
 from torch import Tensor
 import torch
+import torch.nn.functional as F
 from src.dataset.types import BatchedExample
 from src.model.decoder.decoder import DecoderOutput
 from src.model.types import Gaussians
@@ -57,10 +58,27 @@ class LossMse(Loss[LossMseCfg, LossMseCfgWrapper]):
         delta = pred_img - gt_img
 
         # static version, use the static gaussians' rendering, but with the same mask and gt_img
-        pred_img = prediction.static_color.permute(0, 1, 3, 4, 2)[mask] 
-        gt_img = ((batch["context"]["image"][:, batch["using_index"]] + 1) / 2).permute(0, 1, 3, 4, 2)[mask]
+        static_img = prediction.static_color.permute(0, 1, 3, 4, 2)[mask]
+        static_delta = static_img - gt_img
+        static_weight = 0.5
+        
+        # pushing dynamic and static to be away from each other
+        dynamic_img = prediction.dynamic_color.permute(0, 1, 3, 4, 2)[mask]
+        alpha_weight= prediction.dynamic_alpha[mask] * prediction.static_alpha[mask]
+        cos_sim = F.cosine_similarity(
+            dynamic_img, 
+            static_img, 
+            dim=-1
+        ).abs()
+        similarity_loss = (cos_sim * alpha_weight).mean() #(cos_sim * weights).sum() # / (weights.sum() + 1e-6) might need this term for stablizing gradient
+        similarity_weight = 0.01
 
-        static_delta = pred_img - gt_img
+        #print(f"debug: delta-mean: {delta.mean().item()}, static-delta-mean: {static_delta.mean().item()}, dynamic-static-sim: {(similarity_loss * similarity_weight).item()}")
 
-        print(f"debug: delta-mean: {delta.mean().item()}, static-delta-mean: {static_delta.mean().item()}")
-        return self.cfg.weight * (torch.nan_to_num((delta**2).mean(), nan=0.0, posinf=0.0, neginf=0.0) + torch.nan_to_num((static_delta**2).mean(), nan=0.0, posinf=0.0, neginf=0.0))
+
+        combined_loss = torch.nan_to_num((delta**2).mean(), nan=0.0, posinf=0.0, neginf=0.0)
+        static_loss = torch.nan_to_num((static_delta**2 * static_weight).mean(), nan=0.0, posinf=0.0, neginf=0.0)
+        dynamic_sim_loss = torch.nan_to_num(similarity_loss * similarity_weight, nan=0.0, posinf=0.0, neginf=0.0)
+        
+        print(f"debug: mse-loss: {combined_loss.item()}, static-mse-loss: {static_loss.item()}, dynamic-static-sim-loss: {dynamic_sim_loss.item()}")
+        return self.cfg.weight * (combined_loss + static_loss + dynamic_sim_loss)
